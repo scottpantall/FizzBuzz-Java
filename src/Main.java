a84b6b0ae25039d81e95d3fba9ce66bb86dad028
Scott Pantall <aspantall@gmail.com>
2017-04-11 18:26:14 -0600

    Fizz buzz works on CodeEval

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,10 @@
-/*
-Your program should accept a file as its first argument. The file contains
-multiple separated lines; each line contains 3 numbers that are space delimited.
-The first number is the first divider (X), the second number is the second
-divider (Y), and the third number is how far you should count (N). You may 
-assume that the input file is formatted correctly and the numbers are valid 
-positive integers.
-
-Print out the series 1 through N replacing numbers divisible by X with “F”,
-numbers divisible by Y with “B” and numbers divisible by both with “FB”. Since
-the input file contains multiple sets of values, your output should print 
-out one line per set. Ensure that there are no trailing empty spaces in each 
-line you print.
- */
-package fizzbuzz;
-
 import java.io.*;
 
 /**
  *
  * @author Scott
  */
-public class FizzBuzz {
+public class Main {
 
     /**
      * @param args the command line arguments
@@ -58,13 +42,13 @@ public class FizzBuzz {
         // Output
         for(int i = 1; i <= inputLength; i++) {
             if((i % firstDivider == 0) && (i % secondDivider == 0))
-                System.out.print("FB ");
+                System.out.printf("FB ");
             else if(i % firstDivider == 0)
-                System.out.print("F ");
+                System.out.printf("F ");
             else if(i % secondDivider == 0)
-                System.out.print("B ");
+                System.out.printf("B ");
             else
-                System.out.print(i + " ");            
+                System.out.printf(i + " ");            
         }
         System.out.printf("%n");
         
